6dcdf2da96f20987ff0e02ebdcddd629a3f8c3ba
Rodrigo Pérez <rgperez@antel.com.uy>
2019-09-27 16:25:27 -0300

    Agrego cors al uwsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 from app import server
 from db import db
+from flask_cors import CORS
 
 db.init_app(server)
+
+CORS(server,resources={r"/api/*": {"origins": "http://179.27.167.3:3000"}})
 
 @server.before_first_request
 def create_tables():
