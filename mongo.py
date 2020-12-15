a317fde2add9d1e86b5b2666d838bdbbbb8ae9ae
Rodrigo Perez <rgperez@antel.com.uy>
2020-12-15 19:08:06 -0300

    Agrego variables de ambiente para la configuracion de mongo

--- mongo.py
+++ mongo.py
@@ -1,10 +1,19 @@
 from pymongo import MongoClient
 import os
+from urllib import parse
 
-mongo_uri = os.environ.get('MONGO_URI')
+#Mongo URI
+#Example: mongodb://mongoadmin:secret@172.17.0.3:27017
 
-if not(mongo_uri):
-    print(f"Not connection to MongoDB: {mongo_uri}")
+mongo_username = os.environ.get('MONGO_USER')
+mongo_password = os.environ.get('MONGO_PASSWORD')
+mongo_ip = os.environ.get('MONGO_IP')
+mongo_port = os.environ.get('MONGO_PORT')
+
+if not(mongo_username) and (mongo_password) and {mongo_ip} and {mongo_port}:
+    print(f"Not env varible set: {mongo_username} {mongo_password} {mongo_ip} {mongo_port}")
 else:
+
+    mongo_uri = f"mongodb://{parse.quote(mongo_username)}:{parse.quote(mongo_password)}@{mongo_ip}:{mongo_port}/"
     client = MongoClient(mongo_uri)
     db = client.kit
